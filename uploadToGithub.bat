98354c47cc3c7b9a8daa1486d19fabc1021603da
Yenmor <1755170378@qq.com>
2025-11-24 20:03:09 +0800

    a commit

---
+++ uploadToGithub.bat
@@ -0,0 +1,8 @@
+git config --globle user.name "Yenmor"
+git config --globle user.email "yenmor2333@gmail.com"
+git add .
+git commit -m "a commit"
+git remote add origin https://github.com/Yenmor/ObsidianNotes.git
+git branch -M main
+git push -u origin main
+pause
